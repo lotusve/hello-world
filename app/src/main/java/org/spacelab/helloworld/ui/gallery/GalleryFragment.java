2ec69fc9c314070e35cd85bf3705f0a9d1dc3f86
lotusve <lotusve@163.com>
2020-01-13 20:51:18 +0800

    从本地选择图片.

--- app/src/main/java/org/spacelab/helloworld/ui/gallery/GalleryFragment.java
+++ app/src/main/java/org/spacelab/helloworld/ui/gallery/GalleryFragment.java
@@ -1,7 +1,11 @@
 package org.spacelab.helloworld.ui.gallery;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,43 +20,53 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.bumptech.glide.Glide;
 
+import org.spacelab.helloworld.Config.Config;
 import org.spacelab.helloworld.R;
 import org.spacelab.helloworld.data.source.remote.http.gallery.ResponseBean;
+import org.spacelab.helloworld.util.FileUtils;
 
-import java.io.File;
-
+/**
+ * 人脸识别
+ */
 public class GalleryFragment extends Fragment implements View.OnClickListener {
+
+    private static final int REQUEST_PICK_IMAGE = 101;
+
+    private Activity activity;
+
+    private ImageView imageView;
 
     private GalleryViewModel galleryViewModel;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         galleryViewModel = ViewModelProviders.of(this).get(GalleryViewModel.class);
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-
         initView(root);
-
         return root;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        activity = getActivity();
+    }
+
     private void initView(View root) {
+        imageView = root.findViewById(R.id.image);
+        imageView.setOnClickListener(this);
+
         final TextView textView = root.findViewById(R.id.text_gallery);
-        final ImageView imageView = root.findViewById(R.id.image);
         final TextView genderTV = root.findViewById(R.id.gender);
         final TextView ageTV = root.findViewById(R.id.age);
-
         final TextView smileTV = root.findViewById(R.id.smile);
         final TextView emotionTV = root.findViewById(R.id.emotion);
         final TextView beautyTV = root.findViewById(R.id.beauty);
         final TextView skinstatusTV = root.findViewById(R.id.skinstatus);
-
-        imageView.setOnClickListener(this);
 
         galleryViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -67,8 +81,6 @@ public class GalleryFragment extends Fragment implements View.OnClickListener {
 
                 ResponseBean.Face face = responseBean.getFaces().get(0);
 
-                Glide.with(GalleryFragment.this).load(new File(getImageFilePath())).into(imageView);
-
                 genderTV.setText(String.format(getResources().getString(R.string.gender), face.getAttributes().getGender().getValue()));
                 ageTV.setText(String.format(getResources().getString(R.string.age), face.getAttributes().getAge().getValue()));
 
@@ -82,38 +94,60 @@ public class GalleryFragment extends Fragment implements View.OnClickListener {
 
                 ResponseBean.Skinstatus skinstatus = face.getAttributes().getSkinstatus();
                 skinstatusTV.setText(String.format(getResources().getString(R.string.skinstatus), String.valueOf(skinstatus.getHealth()), String.valueOf(skinstatus.getStain()), String.valueOf(skinstatus.getAcne()), String.valueOf(skinstatus.getDark_circle())));
-
             }
         });
 
     }
 
-    private void getData() {
-        galleryViewModel.getData(getImageFilePath());
-    }
-
-    private String getImageFilePath (){
-
-        File storageDirectory = Environment.getExternalStorageDirectory();
-
-        String imageFilePath = "/baidu/searchbox/downloads/1578479381607.jpg";
-        // String imageFilePath = "/DCIM/Camera/IMG_20200113_115331.jpg";
-
-        return storageDirectory + imageFilePath;
+    /**
+     * 获取人脸识别的的结果
+     * @param imageFilePath 人脸图片绝对路径
+     */
+    private void getData(String imageFilePath) {
+        galleryViewModel.getData(imageFilePath);
     }
 
     @Override
     public void onClick(View v) {
-
         switch (v.getId()) {
             case R.id.image:
+                getImageFromLocal();
+                break;
+            default:
+                break;
+        }
+    }
 
-                getData();
+    /**
+     * 从本地相册选择图片
+     */
+    private void getImageFromLocal() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, REQUEST_PICK_IMAGE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+
+        switch (requestCode) {
+            case REQUEST_PICK_IMAGE:
+
+                Uri imageUri = data.getData();
+                Log.d(Config.TAG, "imageUri: " + imageUri);
+
+                Glide.with(activity).load(imageUri).into(imageView);
+
+                String imagePath = FileUtils.getPath(activity, imageUri);
+                Log.d(Config.TAG, "imagePath: " + imagePath);
+
+                getData(imagePath);
 
                 break;
             default:
                 break;
         }
-
     }
 }
