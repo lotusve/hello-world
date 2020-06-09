3a6ca12ce1399982f7708e8037f37d9fa1ffa665
lotusve <lotusve@163.com>
2020-06-09 17:04:23 +0800

    add StringUtil.

---
+++ javalibrary/src/main/java/org/spacelab/javalibrary/utils/StringUtil.java
@@ -0,0 +1,13 @@
+package org.spacelab.javalibrary.utils;
+
+public class StringUtil {
+
+    public static String toUpperCase(String str){
+        return str.toUpperCase();
+    }
+
+    public static String toLowerCase(String str){
+        return str.toLowerCase();
+    }
+
+}
